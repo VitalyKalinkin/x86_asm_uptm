54722fe9a53e4cfa4b4a987e04351e0db1bee87d
Vitalii Kalinkin <vitaly@kalinkin.me>
2020-11-27 17:50:19 -0800

    memory_addressing code

---
+++ x86_asm_uptm/memory_addressing/memory.asm
@@ -0,0 +1,70 @@
+.386
+.model flat, C
+
+.const
+fib_vals dword 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 
+
+; num_fib_vals = sizeof(fib_vals)/sizeof(dword)
+; $ -- offset of the current memory block
+num_fib_vals dword ($-fib_vals)/sizeof dword
+; publishing num_fib_vals
+public num_fib_vals
+
+.code
+memory_addressing PROC
+                  ; prolog
+                  push ebp
+                  mov ebp, esp
+                  push ebx
+                  push esi
+                  push edi
+
+                  ; actual code
+                  xor eax, eax
+                  mov ecx, [ebp + 8] ; ecx = i
+                  cmp ecx, 0         ; 0 - ecx => ZF
+                  jl invalid_index   ; jump if less than
+                  cmp ecx, [num_fib_vals] ; 
+                  jge invalid_index  ; jump if greater or equal than
+
+                  ; v1 -- base register
+                  mov ebx, offset fib_vals   ; ebx = *fib_vals
+                  mov esi, [ebp + 8]         ; esi = i
+				  shl esi, 2                 ; esi = i * 4
+				  add ebx, esi               ; ebx = *fib_vals + i * 4
+				  mov eax, [ebx]             ; eax = fib_vals[i]            
+                  mov edi, [ebp + 12]
+                  mov [edi], eax             ; *v1 = fib_vals[i]
+
+                  ; v2 -- base register + displacement
+				  mov esi, [ebp	+ 8]         ; esi = i
+                  shl esi, 2                 
+                  mov eax, [esi + fib_vals]  ; eax = fib_vals[i]
+                  mov edi, [ebp + 16]       
+                  mov [edi], eax             ; *v2 = fib_vals[i]
+
+                  ; v3 -- base register + index register
+                  mov ebx, offset fib_vals
+                  mov esi, [ebp + 8]
+                  shl esi, 2
+                  mov eax, [ebx + esi]       ; eax, fib_vals[i]
+                  mov edi, [ebp + 20]
+                  mov [edi], eax             ; *v3 = fib_vals[i]
+
+                  ; v4 -- base register + index register * scale_factor
+                  mov ebx, offset fib_vals				  
+                  mov esi, [ebp + 8]        
+                  mov eax, [ebx + esi * 4]
+                  mov edi, [ebp + 24]
+                  mov [edi], eax
+                  mov eax, 1
+
+                  ; epilog
+invalid_index:
+                  pop edi
+                  pop esi
+                  pop ebx
+                  pop ebp
+                  ret
+memory_addressing endp
+end
---
+++ x86_asm_uptm/memory_addressing/memory_addressing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+extern "C" int32_t memory_addressing(int32_t i, int32_t* v1, int32_t* v2, int32_t* v3, int32_t* v4);
+extern "C" int32_t num_fib_vals;
+
+int main()
+{
+    int32_t v1, v2, v3, v4;
+    for (size_t i = 0; i <= static_cast<size_t>(num_fib_vals); i++) {
+        int32_t ret = memory_addressing(i, &v1, &v2, &v3, &v4);
+        std::cout << "ret = " << ret
+            << ", i=" << i
+            << ", v1=" << v1
+            << ", v2=" << v2
+            << ", v3=" << v3
+            << ", v4=" << v4 << std::endl;
+    }
+    return 0;
+}
