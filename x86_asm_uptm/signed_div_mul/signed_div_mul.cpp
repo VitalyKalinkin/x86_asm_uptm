3a364a432ae9072939d0bafd3864dc870dfff489
Vitalii Kalinkin <vitaly@kalinkin.me>
2020-11-27 16:28:24 -0800

    .

--- x86_asm_uptm/signed_div_mul/signed_div_mul.cpp
+++ x86_asm_uptm/signed_div_mul/signed_div_mul.cpp
@@ -5,9 +5,10 @@ extern "C" int32_t integer_mul_div(int32_t a, int32_t b, int32_t * prod, int32_t
 int main()
 {
     int32_t prod, quo, rem;
-    integer_mul_div(21, 9, &prod, &quo, &rem);
+    int32_t ret = integer_mul_div(21, 9, &prod, &quo, &rem);
     std::cout << "Multiplication/Division: " << std::endl;
     std::cout << "  a * b = " << prod << std::endl;
     std::cout << "  a / b = " << quo << std::endl;
     std::cout << "  a % b = " << rem << std::endl;
+    std::cout << "  ret = " << ret << std::endl;
 }
